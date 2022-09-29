a903b9f0485f8256252d2dd79be78c86d3b98eec
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-29 02:26:35 +0200

    Structure files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+         title: 'To-Do List',
+         template: './index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            }
+        ]
+    },
+    devServer: {
+        static: './dist',
+    },
+}
