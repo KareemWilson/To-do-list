2949e89e7ba40d63f727425e88389d3705c32467
KareemWilson <kareemmagdywilson@gmail.com>
2022-10-06 11:11:10 +0200

    Refactoring #3

--- src/index.js
+++ src/index.js
@@ -3,33 +3,31 @@ import { addBtn, input } from './modules/Constants.js';
 import { renderTasks, saveAndRender } from './modules/renderTasks.js';
 import addTask from './modules/addTask.js';
 import './modules/ChangeDOM.js';
+import removeTask from './modules/removeTask.js';
 
-let tasksArr = [];
-if (localStorage.length > 0) {
-  tasksArr = JSON.parse(localStorage.getItem('list'));
-}
+// let tasksArr = [];
+// if (localStorage.length > 0) {
+//   tasksArr = JSON.parse(localStorage.getItem('list'));
+// }
+window.tasksArr = localStorage.length > 0 ? JSON.parse(localStorage.getItem('list')) : [];
 
-renderTasks(tasksArr);
-addBtn.addEventListener('click', () => addTask(input.value, tasksArr));
+renderTasks(window.tasksArr);
+addBtn.addEventListener('click', () => addTask(input.value, window.tasksArr));
 
-window.removeTask = (_id) => {
-  const updatedList = tasksArr.filter((task) => task.id !== _id);
-  tasksArr = updatedList;
-  saveAndRender(tasksArr);
-};
+window.removeTask = (_id) => removeTask(_id);
 
 window.updateDescription = (id, element) => {
-  tasksArr[id].description = element.value;
-  saveAndRender(tasksArr);
+  window.tasksArr[id].description = element.value;
+  saveAndRender(window.tasksArr);
 };
 
 window.clearAllCompleted = () => {
-  const updatedTasks = tasksArr.filter((elem) => !elem.completed);
-  tasksArr = updatedTasks;
-  saveAndRender(tasksArr);
+  const updatedTasks = window.tasksArr.filter((elem) => !elem.completed);
+  window.tasksArr = updatedTasks;
+  saveAndRender(window.tasksArr);
 };
 
 window.changeTaskStatus = (id) => {
-  tasksArr[id].completed = !tasksArr[id].completed;
-  saveAndRender(tasksArr);
+  window.tasksArr[id].completed = !window.tasksArr[id].completed;
+  saveAndRender(window.tasksArr);
 };
